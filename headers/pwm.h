85968dfbf860ffe96ef0348b6e4b4460b6e6a63a
ERNE196077 <ramoscaciqueernesto@hotmail.com>
2015-10-05 16:47:09 -0500

    Started fixing pwm.h and pwm.c after understanding PCI config space architechture

--- headers/pwm.h
+++ headers/pwm.h
@@ -5,12 +5,40 @@
 
 /*****			GPIO BASE REGISTERS			*****/
 
-#define PWM_BLOCK_SIZE			(4*1024)
-#define	PWM_CTL_ADDRESS 	0xFED0C000
+#define 	PWM_BLOCK_SIZE	(4*1024)
+#define		PWM0_PIN_NUM	22
+#define 	PWM0_BASE_ADDR	0x90825000
+#define 	PWM0_CTRL_OFF	(0x0 / 4)
+#define 	PWM0_RES_OFF	(804 / 4)
+#define 	PWM0_GEN_OFF	(808 / 4)
+#define 	PWM1_PIN_NUM	24
+#define		PWM1_BASE_ADDR	0x90827000 
+#define 	PWM1_CTRL_OFF	(0x0 / 4)
+#define 	PWM1_RES_OFF	(804 / 4)
+#define 	PWM1_GEN_OFF	(808 / 4)
 
-#define PWM0_OFFSET			0x0
 
-int 	INIT_PWM			();
-int 	CLOSE_PWM			(int pin_n);
-void 	READ_PWM_REG		(int pin_n);
-u_int16_t pciConfigReadWord (u_int8_t bus, u_int8_t slot, u_int8_t func, u_int8_t offset);
+int 	INIT_PWM0			();
+int 	STOP_PWM0			();
+int 	SET_PWM0_DUTY		(u_int16_t __duty__);
+int 	SET_PWM0_DIV		(u_int8_t __div__);
+void 	PRINT_PWM0_REG		();
+
+int 	INIT_PWM1			();
+int 	STOP_PWM1			();
+int 	SET_PWM1_DUTY		(u_int16_t __duty__);
+int 	SET_PWM1_DIV		(u_int8_t __div__);
+void 	PRINT_PWM1_REG		();
+
+
+
+1. Update License Code wizard
+2. FTP Site
+3. FTP Access Information Window:
+		FTP Site: Paste the gfw1.aus.stglabs.ibm.com (9.3.116.249)
+		Username: --use your own UserID (From LUCI - GSA)---
+		Password: --use your own Password (From LUCI - GSA)---
+
+		Accessing a mounted Discovery CD --UNMARKED--	
+
+		Change directory (copy and paste from the release page e.g. "/afs/austin/projects/gfw/images/firenze/b1001a_1542.840/SIGNED")
