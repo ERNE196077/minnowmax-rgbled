03408cd33ccc2eccb2539a4f74f24e844d00e05d
Ernesto <ramoscaciqueernesto@hotmail.com>
2016-02-07 11:30:22 -0600

    Added bit constructor macros, cleaned from unneeded registers

--- headers/pwm.h
+++ headers/pwm.h
@@ -13,19 +13,21 @@
 
 /*****			PWM MACROS			*****/
 
-#define 	PWM_CTRL_ENABLE(pwm_reg)			pwm_reg |= (1 << 31);
-#define 	PWM_CTRL_DISABLE(pwm_reg)			pwm_reg &= ~(1 << 31);
-#define 	PWM_CTRL_SW_UPDATE(pwm_reg)		pwm_reg |= (1 << 30);
+#define 	PWM_CTRL_ENABLE(pwm_reg)				pwm_reg |= (1 << 31);
+#define 	PWM_CTRL_DISABLE(pwm_reg)				pwm_reg &= ~(1 << 31);
+#define 	PWM_CTRL_SW_UPDATE(pwm_reg)				pwm_reg |= (1 << 30);
 #define 	PWM_CTRL_BASE_UNIT(pwm_reg,value)		pwm_reg &= ~(0xFFFF << 8); pwm_reg |= (value << 8);
-#define 	PWM_CTRL_ON_DIVISOR(pwm_reg,value)	pwm_reg &= ~0xFF; pwm_reg|=value;
+#define 	PWM_CTRL_ON_TIME_DIVISOR(pwm_reg,value)	pwm_reg &= ~0xFF; pwm_reg|=value;
 
 
 /*****			PWM STRUCTURES			*****/
 
 typedef struct {
 	u_int32_t __pwm_ctrl__;
-	u_int32_t __resv_0x0004__[512];
-	u_int32_t __pwm_rest__;
-	u_int32_t __pwm_greg__;
+	#define 	PWM_ENABLE							(1 << 31)			// Construct without this bit for Disable
+	#define 	PWM_SW_UPDATE						(1 << 30)			// Used to update the PWM settings. PWM module will apply the new settings at the end of the current cycle and reset this bit.
+	#define 	PWM_BASE_UNIT(value)				(value << 8)		// Min value = 0x0. Max value 0xFFFF.
+	#define 	PWM_ON_TIME_DIVISOR(value)			(value)				// Min value = 0x0. Max value 0xFF.
+
 }__attribute__ ((packed)) pwm_t;
 
