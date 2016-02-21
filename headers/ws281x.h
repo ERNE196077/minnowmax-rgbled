78260e82771f8280fc37f9eaa4113c44336f8b0e
Ernesto Ramos <ramoscaciqueernesto@hotmail.com>
2016-02-21 14:58:47 -0600

    Adding WS281x functions

--- headers/ws281x.h
+++ headers/ws281x.h
@@ -18,11 +18,38 @@
 
 /*****			WS281X DEFAULT VALUES			*****/
 
-#define WS281X_DEFAULT_PIN			24		// PWM Capable pins: 24 & 26
-#define WS281X_DEFAULT_LED_NUMBER	1		
-#define WS281X_DEFAULT_DMA_CHANNEL	1		
-#define	WS281X_DEFAULT_
+#define WS281X_DEFAULT_GPIO_PIN			24		// PWM Capable pins: 22 & 24
+#define WS281X_DEFAULT_LED_NUMBER		1		
+#define WS281X_DEFAULT_DMA_CHANNEL		0		
+#define	WS281X_DEFAULT_PWM_BASE_UNIT	0x0000
+#define WS281X_DEFAULT_PWM_BIT_HIGH		0x00
+#define WS281X_DEFAULT_PWM_BIT_LOW		0x00
 
 
  /*****			WS281X STRUCTURES			*****/
+
+typedef struct ws281x_devices_t;
+
+typedef struct
+{
+	u_int32_t			__lednumber__:
+	u_int32_t			__gpio_pinnumber__;
+	u_int32_t			__dma_channel__;
+	pwm_fifo_t			__pwm_fifo__;
+	ws281x_devices_t 	__devices__;
+	
+}__attribute__ ((packed)) ws281x_t;
+
+
+/*****			WS281X FUNCTIONS			*****/
+
+int 		ws281x_init					(ws281x_t *ws281x);
+int 		ws281x_deinit				(ws281x_t *ws281x);
+int 		ws281x_devices_map			(ws281x_devices_t *devices);
+void 		ws281x_devices_unmap 		(ws281x_devices_t *devices);
+pwm_fifo_t 	*ws281x_pwmfifo_init		(ws281x_t *ws281x);
+void 		ws281x_pwmfifo_deinit		(ws281x_t *ws281x);
+void		ws281x_pwmfifo_set			(ws281x_t *ws281x);
+void		ws281x_dma_init				(ws281x_devices_t *devices);
+void		ws281x_dma_deint			(ws281x_devices_t *devices);
 
---
+++ ws281x.c
@@ -0,0 +1,44 @@
+
+/*
+ * ws281x.c
+ *
+ * WS281x Functionality
+ * The basic functionality is to set the clock of the PWM as close as possible 
+ * to the frequency specified in the WS281x documentation.
+ * A FIFO is created to contain the registers of the PWM_ON_TIME_DIVISOR which
+ * will be varying to draw the logical bits in the PWM signal.
+ * 
+ * My assumption is that the PWM Controller contains a FIFO which will be fill
+ * with the data from the DMA transfer.
+ *
+ *  Created on: Oct 28, 2015
+ *      Author: minnow
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <linux/pci.h>
+#include "headers/dma.h"
+/*
+
+
+
+
+
+
+
+int 		ws281x_init					(ws281x_t *ws281x);
+int 		ws281x_deinit				(ws281x_t *ws281x);
+int 		ws281x_devices_map			(ws281x_devices_t *devices);
+void 		ws281x_devices_unmap 		(ws281x_devices_t *devices);
+pwm_fifo_t 	*ws281x_pwmfifo_init		(ws281x_t *ws281x);
+void 		ws281x_pwmfifo_deinit		(ws281x_t *ws281x);
+void		ws281x_pwmfifo_set			(ws281x_t *ws281x);
+void		ws281x_dma_init				(ws281x_devices_t *devices);
+void		ws281x_dma_deint			(ws281x_devices_t *devices);
