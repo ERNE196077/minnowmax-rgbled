ba1d2f6848037ba6592c033696af243dd302fd90
Ernesto <minnow@localhost.localdomain>
2016-10-06 16:21:12 -0500

    Adding led structure functionality

--- headers/rgbled_devices.h
+++ headers/rgbled_devices.h
@@ -10,10 +10,13 @@
 
  /*****			RGBLEDS STRUCTURES			*****/
 typedef struct {
-    u_int8_t d;     //dumy variable for 32bit DMA transfer
-	u_int8_t r;
-	u_int8_t g;
-	u_int8_t b;
+    u_int32_t drgb;     //dumy variable for 32bit DMA transfer
+	#define LED_APA102_SET_RED(value)	(value << 16)
+	#define LED_APA102_SET_GREEN(value)	(value << 8)
+	#define LED_APA102_SET_BLUE(value)	(value)
+	#define LED_APA102_MASK_RED			0x00FF0000
+	#define LED_APA102_MASK_GREEN		0x0000FF00
+	#define LED_APA102_MASK_BLUE		0x000000FF
 }__attribute__((packed)) led_apa102_t ;
 
 typedef struct {
