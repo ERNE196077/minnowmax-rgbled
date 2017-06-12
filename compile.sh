337a093e51cbfe47eeab5946308f9033efe541e6
ernestrc <ramoscaciqueernesto@gmail.com>
2017-06-11 20:53:37 -0500

    need to update

--- compile.sh
+++ compile.sh
@@ -4,13 +4,14 @@ echo '#############################'
 echo '#     gcc routine           #'
 echo '#############################'
 echo '#     main.c           #'
-gcc -Wall -g  -c /home/minnow/workspace/minnow-ws281x/main.c -o obj/Debug/main.o
+gcc -Wall -g  -c main.c -o obj/Debug/main.o
 echo '#     rgbled.c           #'
-gcc -Wall -g  -c /home/minnow/workspace/minnow-ws281x/rgbled.c -o obj/Debug/rgbled.o
+gcc -Wall -g  -c rgbled.c -o obj/Debug/rgbled.o
 
-
+echo '#     rgbled_x11.c           #'
+gcc -Wall -g  -c rgbled_x11.c -o obj/Debug/rgbled_x11.o
 echo '#############################'
 echo '#     g++ routine           #'
 echo '#############################'
 
-g++  -o bin/Debug/rgbled obj/Debug/main.o obj/Debug/rgbled.o
+g++  -o bin/Debug/rgbled obj/Debug/main.o obj/Debug/rgbled.o obj/Debug/rgbled_x11.o
