3800e04fda183cf4166f5fe26ed5166b906018f1
ernestrc <ramoscaciqueernesto@hotmail.com>
2017-07-10 01:21:27 -0500

    Migrating to X11 MIT SHM libraries

--- main.c
+++ main.c
@@ -10,15 +10,15 @@ int main (){
 	/*  Run on Minnowboard */
 	rgbled_conf_t ledsconfig = RGBLED_CONF_TYPEAPA102 | 
 							RGBLED_CONF_DMACH(0) | 
-							RGBLED_CONF_LEDNUM(80);
+							RGBLED_CONF_LEDNUM(239);
 
 	if (rgbled_init(&ledsconfig))
 		return 1;
 
 	rgbled_test();
-	sleep(5);
+	sleep(2);
 	
-	rgbled_x11(20,10,20,10,35);
+	rgbled_x11(77,42,78,42,35);
 
 return 0;
 }
